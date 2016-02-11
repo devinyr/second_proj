5fef36aa073af055a54f5f8073b1a3dd61ef1f05
Monty <monty@dnmacbook.hsd1.wa.comcast.net>
2016-02-11 09:48:55 -0800

    use old setting file

--- second_proj/newsettings.py
+++ second_proj/newsettings.py
@@ -75,13 +75,16 @@ WSGI_APPLICATION = 'second_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'second_proj', 
+        'USER': 'second_proj_name', 
+        'PASSWORD': 'second_proj', 
+        'HOST': 'localhost', 
+        'PORT': '', 
+    } 
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -101,3 +104,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
