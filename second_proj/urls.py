6fe0b46c46796054b6f9d2c24f7ba16b2f1b6c22
Monty <monty@dnmacbook.hsd1.wa.comcast.net>
2016-02-18 09:30:54 -0800

    cleaning up directory structure

--- second_proj/urls.py
+++ second_proj/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     
     url(r'admin/', include(admin.site.urls)),
-    url(r'^bltest/', include('apps.bltest.urls')),
+    url(r'^$', include('apps.bltest.urls')),
 ]
